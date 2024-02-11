03ccdbecd87125eab2b9d75ff08d06fe24673316
Danny Grigorov (yoreei) <oddnecessity@gmail.com>
2024-02-11 22:16:58 +0200

    spawn a cube at 0,0,0

---
+++ Source/UE5TopDownARPG/UE5TopDownARPGGameMode.cpp
@@ -0,0 +1,60 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5TopDownARPGGameMode.h"
+#include "UE5TopDownARPGPlayerController.h"
+#include "UE5TopDownARPGCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+#include "UE5TopDownARPG.h"
+
+AUE5TopDownARPGGameMode::AUE5TopDownARPGGameMode()
+{
+	// use our custom PlayerController class
+	PlayerControllerClass = AUE5TopDownARPGPlayerController::StaticClass();
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
+	if (PlayerPawnBPClass.Class != nullptr)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+
+	// set default controller to our Blueprinted controller
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
+	if(PlayerControllerBPClass.Class != NULL)
+	{
+		PlayerControllerClass = PlayerControllerBPClass.Class;
+	}
+}
+
+void AUE5TopDownARPGGameMode::EndGame(bool IsWin)
+{
+	if (IsWin)
+	{
+		UE_LOG(LogUE5TopDownARPG, Log, TEXT("Win"));
+	}
+	else
+	{
+		UE_LOG(LogUE5TopDownARPG, Log, TEXT("Lose"));
+	}
+}
+
+void AUE5TopDownARPGGameMode::StartPlay()
+{
+	Super::StartPlay();
+
+	UWorld* pWorld = GetWorld();
+	ensure(pWorld);
+
+	generateMaze();
+
+	spawnMaze();
+
+	FActorSpawnParameters SpawnParameters;
+	SpawnParameters.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+	FVector Location{ 0.f,0.f,0.f };
+	FRotator Rotation{0.f,0.f,0.f};
+	AActor* SpawnedActor = GetWorld()->SpawnActor(WallClass, &Location, &Rotation, SpawnParameters);
+
+	ensure(SpawnedActor);
+}
