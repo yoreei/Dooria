328c811af0cd545fd993252df57cd31079bc5e43
Danny Grigorov (yoreei) <oddnecessity@gmail.com>
2024-02-18 13:07:57 +0200

    traps now do damage

--- Source/UE5TopDownARPG/Trigger/FloorTrapTrigger.h
+++ Source/UE5TopDownARPG/Trigger/FloorTrapTrigger.h
@@ -1,17 +1,17 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
-
-#include "CoreMinimal.h"
-#include "DamageTrigger.h"
-#include "FloorTrapTrigger.generated.h"
-
-/**
- * 
- */
-UCLASS()
-class UE5TOPDOWNARPG_API AFloorTrapTrigger : public ADamageTrigger
-{
-	GENERATED_BODY()
-	
-};
+//// Fill out your copyright notice in the Description page of Project Settings.
+//
+//#pragma once
+//
+//#include "CoreMinimal.h"
+//#include "DamageTrigger.h"
+//#include "FloorTrapTrigger.generated.h"
+//
+///**
+// * 
+// */
+//UCLASS()
+//class UE5TOPDOWNARPG_API AFloorTrapTrigger : public ADamageTrigger
+//{
+//	GENERATED_BODY()
+//	
+//};
